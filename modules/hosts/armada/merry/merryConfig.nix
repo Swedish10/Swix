e586f7703199fc3b172d693a6a431e2181ffefcb
swedish <swedish@swixWSL.localdomain>
2026-07-17 04:17:35 +0000

    configuration for macmini, merry

---
+++ modules/hosts/armada/merry/merryConfig.nix
@@ -0,0 +1,14 @@
+{ self, inputs, ... }: {
+  flake.nixosConfigurations.merry = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.armadaBase
+      self.nixosModules.user
+      self.nixosModules.corePrograms
+      self.nixosModules.nh
+      self.nixosModules.nixvim
+      self.nixosModules.console
+      self.nixosModules.fonts
+      self.nixosModules.macminiWireless
+    ];
+  };
+}
